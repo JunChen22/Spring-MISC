7b458d9863ac057c8004df4f040e3359b5685eca
Jun Hao Chen <ccjh522@gmail.com>
2020-03-07 20:52:49 -0800

    Add files via upload

---
+++ filestore-database/src/main/java/com/example/filestore/database/filestoredatabase/FilestoreDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.filestore.database.filestoredatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilestoreDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilestoreDatabaseApplication.class, args);
+    }
+
+}
---
+++ filestore-database/src/test/java/com/example/filestore/database/filestoredatabase/FilestoreDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filestore.database.filestoredatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilestoreDatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
